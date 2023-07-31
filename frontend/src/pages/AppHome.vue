0f083327adce8802e40d9555bff91093872e1282
michele <michele.giovagnoli1@gmail.com>
2023-07-31 16:11:59 +0200

    Aggiornamento: Home, Inizio sezione espereienze.

--- frontend/src/pages/AppHome.vue
+++ frontend/src/pages/AppHome.vue
@@ -1,6 +1,32 @@
 <template>
     <div class="Home-page container">
-        <h1>Work in progress</h1>
+        <section id="experience" class="mb-0">
+            <h3>Esperienze.</h3>
+            <div class="color-white ps-6 pb-4 position-relative">
+                <div class="bar"></div>
+                <i class="fas fa-circle opacity-8 dot-experience"></i>
+                <h3 class="font-14 color-white opacity-8 mb-0">Boolean careers</h3>
+                <h4 class="opacity-8 fw-light font-12 mb-0">Full Stack Web Developer Trainee</h4>
+                <p class="opacity-5 fw-light mb-0">gennaio 2023 - giugno 2023</p>
+                <p class="opacity-5 fw-light">Milano, Lombardia, Italia</p>
+                <p class="fw-normal mb-0">
+                    Durante questi 6 mesi ho maturato 700 ore tra teoria e pratica sviulppando famose Web Applications come
+                    <br>
+                    Netflix, Whatsapp Web, Spotify e molti altri.
+                    <br>
+                    Le principali tecnologie che ho utilizzato sono:
+                </p>
+                <ul class="list-style">
+                    <li>HTML</li>
+                    <li>CSS (Sass)</li>
+                    <li>Javascript (Vue, Axios, JQuery)</li>
+                    <li>PHP</li>
+                    <li>MySql</li>
+                    <li>Laravel</li>
+                </ul>
+            </div>
+        </section>
+        <h1>Skills.</h1>
     </div>
 </template>
 <script>
